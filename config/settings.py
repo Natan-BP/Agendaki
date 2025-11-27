162c8a1b89bf4c7ad7526608a712a230390fad5e
SonecaApenas <giovanni_lasalvia@usp.br>
2025-11-27 17:22:31 -0300

    fix render

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -130,7 +131,7 @@ STATIC_URL = 'static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
